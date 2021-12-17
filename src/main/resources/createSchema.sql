e240bf3bf11cf07c86f27e764179c436681c0947
Erez <erezkatza@gmail.com>
2021-12-18 00:20:44 +0200

    Added stocks table

--- src/main/resources/createSchema.sql
+++ src/main/resources/createSchema.sql
@@ -12,3 +12,21 @@ CREATE TABLE `StockHolmes_dev`.`exchanges` (
   `end_hour` TIME NOT NULL,
   PRIMARY KEY (`id`),
   UNIQUE INDEX `id_UNIQUE` (`id` ASC) VISIBLE);
+
+  CREATE TABLE `StockHolmes_dev`.`stocks` (
+    `id` INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(45) NULL,
+    `ticker` VARCHAR(6) NOT NULL,
+    `description` VARCHAR(45) NULL,
+    `isActive` BOOLEAN NOT NULL,
+    `listingDate` DATETIME NULL,
+    `delistingDate` DATETIME NULL,
+    `exchangeId` INT NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE INDEX `ticker_UNIQUE` (`ticker` ASC) VISIBLE,
+    INDEX `stock_exchangeId_FK_idx` (`exchangeId` ASC) VISIBLE,
+    CONSTRAINT `stocks_exchangeId_FK`
+      FOREIGN KEY (`exchangeId`)
+      REFERENCES `StockHolmes_dev`.`exchanges` (`id`)
+      ON DELETE NO ACTION
+      ON UPDATE NO ACTION);
